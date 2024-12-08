4856d43b67fb58b3c952520cae5b38b83bb94ea1
takonomura <takonomura@users.noreply.github.com>
2024-12-08 15:37:59 +0900

    Remove join for ride_statuses

--- webapp/go/owner_handlers.go
+++ webapp/go/owner_handlers.go
@@ -122,7 +122,7 @@ func ownerGetSales(w http.ResponseWriter, r *http.Request) {
 	modelSalesByModel := map[string]int{}
 	for _, chair := range chairs {
 		rides := []Ride{}
-		if err := tx.SelectContext(ctx, &rides, "SELECT rides.* FROM rides JOIN ride_statuses ON rides.id = ride_statuses.ride_id WHERE chair_id = ? AND status = 'COMPLETED' AND updated_at BETWEEN ? AND ? + INTERVAL 999 MICROSECOND", chair.ID, since, until); err != nil {
+		if err := tx.SelectContext(ctx, &rides, "SELECT rides.* FROM rides WHERE chair_id = ? AND evaluation IS NOT NULL AND updated_at BETWEEN ? AND ? + INTERVAL 999 MICROSECOND", chair.ID, since, until); err != nil {
 			writeError(w, http.StatusInternalServerError, err)
 			return
 		}
