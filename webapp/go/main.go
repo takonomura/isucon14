70eef3b3503599d0cb4b7540fb44854b60454af9
takonomura <takonomura@users.noreply.github.com>
2024-12-08 14:52:21 +0900

    Disable chi logger

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -67,7 +67,6 @@ func setup() http.Handler {
 	db = _db
 
 	mux := chi.NewRouter()
-	mux.Use(middleware.Logger)
 	mux.Use(middleware.Recoverer)
 	mux.HandleFunc("POST /api/initialize", postInitialize)
 
