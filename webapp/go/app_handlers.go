faec60c674f0f8386d0014eaab666284ca69fdbd
takonomura <takonomura@users.noreply.github.com>
2024-12-08 16:56:07 +0900

    Increase retry after ms for app notification

--- webapp/go/app_handlers.go
+++ webapp/go/app_handlers.go
@@ -716,7 +716,7 @@ func appGetNotification(w http.ResponseWriter, r *http.Request) {
 			CreatedAt: ride.CreatedAt.UnixMilli(),
 			UpdateAt:  ride.UpdatedAt.UnixMilli(),
 		},
-		RetryAfterMs: 30,
+		RetryAfterMs: 500,
 	}
 
 	if ride.ChairID.Valid {
